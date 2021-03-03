a627c0bf89132cb5c1e8084dd408bed7aac431fb
jasmynej14 <jasmynej14@brandeis.edu>
2021-03-02 19:09:06 -0500

    feed item,but there are  issues w/displaying

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,27 +1,68 @@
 <template>
-    <ScrollView orientation="vertical" class="feed-view">
-        <StackLayout orientation="vertical">
-            <FeedItem/>
-        </StackLayout>
-    </ScrollView>
+   <ListView for="item in items" @itemTap="click">
+            <v-template>
+                <FeedItem :title="post_title" :author="poster"/>
+            </v-template>
+    </ListView>
+    
 </template>
 
 <script>
-import FeedItem from './FeedItem'
+import FeedItem from './FeedItem';
+import posts from '../data/posts.json';
+import users from '../data/users.json';
 export default {
     
     data(){
         return {
-            title: "Feed"
+            post_title: this.getTitle(),
+            poster: this.getName(),
+            items: this.gatherFeed()
         }
     },
     components:{
         FeedItem
+    },
+    methods:{
+        getTitle(){
+            const post_id = Math.floor(Math.random() * 100) - 1
+            
+            return posts[post_id.toString()]["title"]
+        },
+        getName(){
+            const user_id = Math.floor(Math.random() * 49) - 1
+            return users[user_id.toString()]["name"]
+        },
+        gatherFeed(){
+            var feeditems = []
+            var titles = []
+            var i;
+            for(i =0;i<20;i++){
+                const post = { title:this.getTitle(), poster:this.getName() }
+                feeditems.push(post)
+                titles.push(this.getTitle())
+            }
+            
+            return feeditems;
+        },
+        click(){
+            confirm({
+                title:"Go To Post?",
+                message:"Going to watch debate",
+                okButtonText: "Yes!",
+                cancelButtonText: "Not Yet."
+            }).then(result =>{
+                console.log(result)
+            });
+        }
     }
 }
 </script>
 
 <style scoped>
+    .words{
+        color:black;
+    }
     .feed-view{
         background-color: beige;
     }
--- src/components/FeedItem.vue
+++ src/components/FeedItem.vue
@@ -1,15 +1,35 @@
 <template>
     <StackLayout class="post">
-        <Label class="title" text="Post title"/>
-        <Label class="user" text="Poster"/>
+        <Label class="title" :text="title"/>
+        <Label class="user" :text="author"/>
       
     </StackLayout>
 </template>
 
 <script>
-   
+ import posts from '../data/posts.json'
+ import users from '../data/users.json'
 export default {
-    
+    data(){
+        return{
+        }
+       
+    },
+
+    props:{
+        title: String,
+        author: String
+    },
+    methods:{
+        getTitle(){
+            const id = Math.floor(Math.random() * 100) - 1
+            return posts[id]["title"]
+        },
+        getName(){
+            const id = Math.floor(Math.random() * 50) - 1
+            return users[id]["name"]
+        }
+    }
 }
 </script>
 
